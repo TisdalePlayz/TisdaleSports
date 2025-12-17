318c1581558143688f09c837d1d00522ad71f326
TisdalePlayz <brotherxiownage35@gmail.com>
2025-12-16 19:01:52 -0600

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>TisdaleSports</h1>
+      <p>If you see this, your deploy worked.</p>
+    </main>
+  );
+}
